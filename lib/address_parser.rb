8f57b54990dbf40a4c6180e9df998cd2f8e811b6
Ruslan Yakhyaev <ruslan@ruslan.io>
2015-10-19 13:50:49 +0200

    First iteration of address parser

--- lib/address_parser.rb
+++ lib/address_parser.rb
@@ -1,5 +1,76 @@
-require "address_parser/version"
+require 'address_parser/version'
+require 'address_parser/states'
+require 'address_parser/street_types'
 
 module AddressParser
-  # Your code goes here...
+  class Base
+    include States
+    include StreetTypes
+
+    def initialize(address)
+      @address                = address
+
+      @state_pattern       = nil
+      @postcode_pattern    = %r{\s?(?<postcode>\d+)$}
+      @unit_number_pattern = %r{
+        ^\D*?
+        (?:(?<unit>\d+(?=[\/|\s+]))[\/|\s+])?
+        (?<number>(?:\d|-)+)
+      }x
+
+      @result = {}
+    end
+
+    def process_address
+      puts @address
+
+      extract_state
+      extract_postcode
+      extract_unit_and_number
+      extract_street_and_type
+      extract_suburb
+
+      @result
+    end
+
+    def extract_postcode
+      match              = @postcode_pattern.match(@address)
+      @address           = @address.sub(@postcode_pattern, '')
+      @result[:postcode] = match && match[:postcode]
+    end
+
+    def extract_state
+      match = STATES.flatten.detect do |s|
+        @state_pattern  = Regexp.new("\\s+#{s}\\s+", Regexp::IGNORECASE)
+        @address       =~ @state_pattern
+      end
+
+      @address = @address.sub(@state_pattern, ' ')
+      @result[:state] = STATES.select { |s| s.include?(match) }.flatten[0]
+    end
+
+    def extract_street_and_type
+      type_match = STREET_TYPES.flatten.detect do |st|
+        @street_pattern = Regexp.new("(?<street>\\w+)\\s+#{st}", Regexp::IGNORECASE)
+        @address       =~ @street_pattern
+      end
+
+      street_match          = @street_pattern.match(@address)
+      @address              = @address.sub(@street_pattern, '')
+      @result[:street]      = street_match && street_match[:street]
+      @result[:street_type] = type_match
+    end
+
+    def extract_suburb
+      @address = @address.gsub(/[^\w\s]/, '').strip
+      @result[:suburb] = @address
+    end
+
+    def extract_unit_and_number
+      match            = @unit_number_pattern.match(@address)
+      @address         = @address.sub(@unit_number_pattern, '')
+      @result[:unit]   = match && match[:unit]
+      @result[:number] = match && match[:number]
+    end
+  end
 end
--- test/address_parser_test.rb
+++ test/address_parser_test.rb
@@ -1,11 +1,48 @@
 require 'test_helper'
 
-class AddressParserTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::AddressParser::VERSION
-  end
+class AddressParserBaseTest < Minitest::Test
+  def test_should_extract_address
+    puts AddressParser::Base.new('9/24-40 Springfield Avenue, Potts Point NSW 2011')
+      .process_address
 
-  def test_it_does_something_useful
-    assert false
+    puts '---'
+
+    puts AddressParser::Base.new('7/329 Bondi Road, Bondi Beach NSW 2026')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('158-166 Day Street, Sydney NSW 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('C203 Globe Street, Sydney NSW 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('Globe street QLD 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('SYDNEY victoria 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('Shop 1 82 Elizabeth Street, Sydney NSW 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('2 BOND STREET, SYDNEY 2000')
+      .process_address
+
+    puts '---'
+
+    puts AddressParser::Base.new('Some street, NSW ')
+      .process_address
   end
 end
